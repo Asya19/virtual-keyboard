27345b06d2b473ec8d65462e630b8999ca2da933
Asya19 <asya.kisko@yandex.ru>
2022-05-04 15:14:19 +0300

    feat: change url for fonts

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// // Массив знаков на два языка
-// const en = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'];
-// const ru = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', '\\', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'];
-
-// // Массив знаков, когда зажат SHIFT
-// const en_shift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'];
-// const ru_shift = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', '\\', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '&larr;', '&darr;', '&rarr;'];
-
-// const keyCodes = ['apostrophe', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-// const classForLengthBnt = ['', '', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', ' specialBtn', '', '', '', '', '', '', '', '', '', '', '', '', '', 'last specialBtn', 'twoBtnLength specialBtn', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', 'twoBtnLength specialBtn', '', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', 'twoBtnLength specialBtn', 'specialBtn', 'specialBtn', 'spaceBtn', 'specialBtn', 'twoBtnLength specialBtn', '', '', 'last'];
-
 // Массив знаков на два языка
 const en = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
 const ru = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
@@ -30,39 +19,33 @@ let lang;
 
 if (localStorage.getItem('cur_language') !== undefined) lang = localStorage.getItem('cur_language'); else lang = 'en';
 
-
+function addListeners() {
+  document.addEventListener('mousedown', (event) => onMouseDown(event));
+}
 
 function CreateKeyboard() {
   let content = `<textarea></textarea>
                  <div class="keyboard"></div>`;
-                //  console.log(content);
-
   BODY.insertAdjacentHTML('afterbegin', content);
   let KEYBOARD = document.querySelector('.keyboard');
-
 
   for (let i in classForLengthBnt) {
     const addClass = classForLengthBnt[i];
     let content = `<div class="oneBtn ${addClass}">
-                <span data-code="${keyCodes[i]}">${getLayOutKeyboard(i)}</span>
-              </div>`;
+                      <span data-code="${keyCodes[i]}">${getLayOutKeyboard(i)}</span>
+                   </div>`;
     KEYBOARD.insertAdjacentHTML('beforeend', content);
   };
   let mes = `<p>Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe Shift + Ctrl<\p>`;
   BODY.insertAdjacentHTML('beforeend', mes);
 }
 
-
 function changeKeyboardLayout() {
   const spans = document.querySelectorAll('span');
-  for (let i = 0; i < spans.length; i++) {
-    spans[i].innerHTML = getCurrentKeyByCode(i);
-    // console.log(spans.length);
-  }
+  for (let i = 0; i < spans.length; i++) spans[i].innerHTML = getCurrentKeyByCode(i);
 }
 
 function getCurrentKeyByCode(i) {
-  // const len = KEY_CODES.length;
   let index = 0;
   for (let i in keyCodes) {
     if (keyCodes[i] === i) {
@@ -73,7 +56,15 @@ function getCurrentKeyByCode(i) {
   return getLayOutKeyboard(index);
 }
 
+// Функция нажатия мышкой и клавиатурой
 
+function onMouseDown(event) {
+  event.preventDefault();
+
+  console.log('HI');
+
+
+}
 
 
 
@@ -85,10 +76,8 @@ function getLayOutKeyboard(i) {
   if (lang === 'en') return en[i];
   return ru[i];
 }
-
 window.onload = () => {
   CreateKeyboard();
-  // changeKeyboardLayout();
   if (localStorage.getItem('cur_language') !== undefined) lang = localStorage.getItem('cur_language'); else lang = 'en';
 };
 
