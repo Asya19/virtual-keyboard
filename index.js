846156cee2292a54de18d336d6777bb7a6150d83
Asya19 <asya.kisko@yandex.ru>
2022-05-06 20:32:43 +0300

    feat: create Caps and Shift function

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ window.onload = () => {
   const ru = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
 
   // Массив знаков, когда зажат SHIFT
-  const en_shift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
-  const ru_shift = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
+  const en_shift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
+  const ru_shift = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
 
   const keyCodes = ['Apostrophe', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
-  const classForLengthBnt = ['', '', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', ' specialBtn', '', '', '', '', '', '', '', '', '', '', '', '', '', 'last specialBtn', 'twoBtnLength specialBtn', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', 'twoBtnLength specialBtn', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', 'twoBtnLength specialBtn', 'specialBtn', 'specialBtn', 'spaceBtn', 'specialBtn', '', '', '', 'specialBtn last'];
+  const classForLengthBnt = ['', '', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', ' specialBtn', '', '', '', '', '', '', '', '', '', '', '', '', '', 'last specialBtn', 'twoBtnLength specialBtn capsButton', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn', 'twoBtnLength specialBtn key_leftShift', '', '', '', '', '', '', '', '', '', '', '', 'twoBtnLength last specialBtn key_rightShift', 'twoBtnLength specialBtn', 'specialBtn', 'specialBtn', 'spaceBtn', 'specialBtn', '', '', '', 'specialBtn last'];
 
   const BODY = document.querySelector('body');
 
@@ -20,21 +20,48 @@ window.onload = () => {
   let caps = 'off';
   let shift = false;
 
-  // let text;
-  // let cursor;
-  
-
-  let textArea = document.querySelector('.textArea');
-  text = document.querySelector('.textarea');
-
 
 
   CreateKeyboard();
-  // addListeners();
 
-  // text.innerHTML = 'hisasdasda';
-  // cursor = text.selectionStart;
-  // cursor = text.selectionEnd;
+
+  // должно быть в классе
+  function CreateKeyboard() {
+    languageCheck();
+    let content = `<textarea class="textArea" name="text" autofocus></textarea>
+                  <div class="keyboard"></div>`;
+    BODY.insertAdjacentHTML('afterbegin', content);
+
+
+
+    let KEYBOARD = document.querySelector('.keyboard');
+
+    for (let i in classForLengthBnt) {
+      const addClass = classForLengthBnt[i];
+      // let content = `<div class="oneBtn key ${addClass}" data-code="${keyCodes[i]}" data-ru-shift="${ru[i]}" data-en-shift="${en[i]}">
+      //                   <span>${getLayOutKeyboard(i)}</span>
+      //               </div>`;
+      let content = `<div class="oneBtn key ${addClass}" data-code="${keyCodes[i]}" data-ru-shift="${ru_shift[i]}" data-en-shift="${en_shift[i]}" data-ru="${ru[i]}" data-en="${en[i]}">
+      ${getLayOutKeyboard(i)}
+                    </div>`;
+      KEYBOARD.insertAdjacentHTML('beforeend', content);
+    };
+    let mes = `<p>Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe Shift + Ctrl<\p>`;
+    BODY.insertAdjacentHTML('beforeend', mes);
+  }
+
+
+  document.querySelector('.keyboard').addEventListener('click', (event) => {
+    if (event.target.closest('.key')) {
+      const currentButton = event.target.closest('.key');
+
+      if (currentButton.dataset.code === 'ShiftLeft' || currentButton.dataset.code === 'ShiftRight') {
+        shift = !shift;
+        currentButton.classList.toggle('active');
+      }
+      keyPress(event, currentButton, currentButton.dataset.code);
+    }
+  });
 
   function languageCheck() {
     if (localStorage.getItem('lang')) {
@@ -42,6 +69,15 @@ window.onload = () => {
     } else {
       localStorage.setItem('lang', lang);
     }
+  }
+
+  function getLayOutKeyboard(i) {
+    if (shiftState === true || capsState === true) {
+      if (lang === 'en') return en_shift[i];
+      return ru_shift[i];
+    }
+    if (lang === 'en') return en[i];
+    return ru[i];
   }
 
   function languageChange(event) {
@@ -55,113 +91,163 @@ window.onload = () => {
   }
 
 
+  // function changeKeyboardLayout() {
+  //   const spans = document.querySelectorAll('span');
+  //   for (let i = 0; i < spans.length; i++) spans[i].innerHTML = getCurrentKeyByCode(i);
+  // }
 
-  document.querySelector('.keyboard').addEventListener('click', (event) => {
-    // console.log(event);
+  // function getCurrentKeyByCode(i) {
+  //   let index = 0;
+  //   for (let i in keyCodes) {
+  //     if (keyCodes[i] === i) {
+  //       index = i;
+  //       break;
+  //     }
+  //   }
+  //   return getLayOutKeyboard(index);
+  // }
 
-    if (event.target.closest('.key')) {
+  function keyPress(event, button, dataCode) {
 
-      const currentButton = event.target.closest('.key');
-
-      if (currentButton.dataset.code === 'ShiftLeft' || currentButton.dataset.code === 'ShiftRight') {
-        shift = !shift;
-        console.log(shift);
-        currentButton.classList.toggle('active');
-      }
-      keyPress(event, currentButton, currentButton.dataset.code);
-    }
-
-  });
-
-
-  // должно быть в классе
-  function CreateKeyboard() {
-    languageCheck();
-    let content = `<textarea class="textArea" name="text" autofocus></textarea>
-                  <div class="keyboard"></div>`;
-    BODY.insertAdjacentHTML('afterbegin', content);
-    let KEYBOARD = document.querySelector('.keyboard');
-
-    for (let i in classForLengthBnt) {
-      const addClass = classForLengthBnt[i];
-      let content = `<div class="oneBtn key ${addClass}" data-code="${keyCodes[i]}"  data-ru-shift="${getLayOutKeyboard(i)}" data-en-shift="${getLayOutKeyboard(i)}">
-                        <span>${getLayOutKeyboard(i)}</span>
-                    </div>`;
-      KEYBOARD.insertAdjacentHTML('beforeend', content);
-    };
-    let mes = `<p>Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe Shift + Ctrl<\p>`;
-    BODY.insertAdjacentHTML('beforeend', mes);
-  }
-  function changeKeyboardLayout() {
-    const spans = document.querySelectorAll('span');
-    for (let i = 0; i < spans.length; i++) spans[i].innerHTML = getCurrentKeyByCode(i);
-  }
-  function getCurrentKeyByCode(i) {
-    let index = 0;
-    for (let i in keyCodes) {
-      if (keyCodes[i] === i) {
-        index = i;
-        break;
-      }
-    }
-    return getLayOutKeyboard(index);
-  }
-  function getLayOutKeyboard(i) {
-    if (shiftState === true || capsState === true) {
-      if (lang === 'en') return en_shift[i];
-      return ru_shift[i];
-    }
-    if (lang === 'en') return en[i];
-    return ru[i];
-  }
-
-  function keyPress(event, button, keyCode) {
+    let textArea = document.querySelector('.textArea');
     // console.log('hi keyPress');
     let text = '';
-    // let cursor = textArea.selectionStart;
+    let cursor = textArea.selectionStart;
+    textArea.focus();
     event.preventDefault();
-    // textArea.focus();
 
-    if (keyCode === 'CapsLock') changeCapsLock(event);
+    if (dataCode === 'CapsLock') changeCapsLock(event);
+    if (dataCode === 'Tab') text = '    ';
+    if (dataCode === 'Tab') text = '\n';
+    if (dataCode === 'Tab') text = '-1';
+    if (dataCode === 'ShiftLeft' || dataCode === 'ShiftRight') updateKeyboard(event);
+    if (dataCode === 'ArrowLeft' && cursor > 0) textArea.setSelectionRange(cursor - 1, cursor - 1);
 
+    if ((dataCode === 'AltLeft' && (event.shiftKey || shift)) || (dataCode === 'AltRight' && (event.shiftKey || shift)) || 
+        (dataCode === 'ShiftLeft' && event.altKey) || (dataCode === 'ShiftRight' && event.altKey)) {
+      languageChange(event);
+      removeShift(event);
+    }
 
+    if (dataCode === 'ArrowRight') {
+      console.log('ArrowRight');
+      cursor = textArea.selectionEnd;
+      textArea.setSelectionRange(cursor + 1, cursor + 1);
+    }
 
+    if (dataCode === 'ArrowUp') {
+      const textBeforeCursor = textArea.value.substring(0, cursor).split('\n');
+
+      // если нет вводов или строка длиннее 57 символов
+      if (textBeforeCursor.length === 1 || textBeforeCursor[textBeforeCursor.length - 1].length >= 57) 
+        cursor = -57;
+
+      // если предыдущая строка длиннее позиции курсора в текущей строке
+      else if (textBeforeCursor[textBeforeCursor.length - 1].length <= textBeforeCursor[textBeforeCursor.length - 2].length % 57)
+        cursor -= (textBeforeCursor[textBeforeCursor.length - 2].length % 57) + 1;
+      
+      else
+        cursor -= textBeforeCursor[textBeforeCursor.length - 1].length + 1
+      
+      if (cursor < 0) cursor = 0;
+      textArea.setSelectionRange(cursor, cursor);
+    }
+
+    if (dataCode === 'ArrowDown') {
+      cursor = textArea.selectionEnd;
+
+      const textBeforeCursor = textArea.value.substring(0, cursor).split('\n');
+      const textAfterCursor = textArea.value.substring(textArea.selectionEnd).split('\n');
+
+      // если нет вводов или следующая строка длиннее 57 символов
+      if (textAfterCursor.length === 1 || textAfterCursor[0].length >= 57) cursor += 57;
+
+      // если следующая строка короче позиции курсора
+      else if ((textBeforeCursor[textBeforeCursor.length - 1].length % 57) > textAfterCursor[1].length)
+        cursor += textAfterCursor[0].length + textAfterCursor[1].length + 1;
+
+      // если текущая строка очень длинная
+      else if ((((textBeforeCursor[textBeforeCursor.length - 1].length) + textAfterCursor[0].length) > 57))
+        cursor += textAfterCursor[0].length;
+
+      // если следующая строка длиннее позиции курсора
+      else
+        cursor += (textBeforeCursor[textBeforeCursor.length - 1].length % 57) + textAfterCursor[0].length + 1;
+
+      textArea.setSelectionRange(cursor, cursor);
+
+    }
+
+    // textArea.innerHTML = 'ddd';
+    if (text) {
+      
+      let textAfterCursor = textArea.value.substring(textArea.selectionEnd);
+      let textBeforeCursor = textArea.value.substring(0, cursor);
+
+      if (text === '-1') {
+        text = '';
+
+        if (cursor === textArea.selectionEnd) {
+          textBeforeCursor = textBeforeCursor.slice(0, -1);
+          cursor -= (cursor > 0) ? 2 : 1;
+        } else cursor -= 1;
+
+      }
+      textArea.innerText = textBeforeCursor + text + textAfterCursor;
+      textArea.setSelectionRange(cursor++, cursor++);
+      if (text === '    ') textArea.setSelectionRange(cursor + 4, cursor + 4);
+    }
+    
+    if (!button.classList.contains('specialBtn')) {
+      text = button.textContent;
+      removeShift(event);
+    }
   }
   // должно быть в классе
   function changeCapsLock(event) {
-    if (caps === 'on') {
-      caps = 'off';
-    } else {
-      caps = 'on';
-    }
+    if (caps === 'on') caps = 'off';
+    else caps = 'on';
+
+    capsLock();
     updateKeyboard(event);
   }
 
-   // надо будет написать эту функцию
-          // Update keyboard if switch language or pressing Shift
-          // updateKeyboard(event)
-
   function updateKeyboard(event) {
-    // const { lang } = this;
-    // console.log(event);
-
     if (event.shiftKey || shift) {
       document.querySelectorAll('.key').forEach((e) => {
-        
-        if (e.dataset[`${lang}Shift`]) {
 
-          // if (caps === 'on') {
-            
-          // }
-
+        if (e.dataset[`${lang}Shift`] && !(e.classList.contains('specialBtn'))) {
+          if (caps === 'on' && !(e.classList.contains('specialBtn'))) e.innerHTML = e.dataset[`${lang}Shift`].toLowerCase();
+          else e.innerHTML = e.dataset[`${lang}Shift`];
         }
-
-
+        else if (e.dataset[lang] && !(e.classList.contains('specialBtn'))) e.innerHTML = e.dataset[lang];
       });
     }
-
+    else {
+      document.querySelectorAll('.key').forEach((e) => {
+        if (e.dataset[lang] && !(e.classList.contains('specialBtn'))) {
+          if (caps === 'on' && !(e.shiftKey || shift) && !(e.classList.contains('specialBtn'))) 
+            e.innerHTML = e.dataset[lang].toUpperCase();
+          else e.innerHTML = e.dataset[lang];
+        }
+      });
+    }
   }
 
+  function removeShift(event) {
+    if (shift) {
+      shift = !shift;
+      document.querySelector('.key_leftShift').classList.remove('active');
+      document.querySelector('.key_rightShift').classList.remove('active');
+      updateKeyboard(event);
+    }
+  }
+
+  // меняет цвет текста capsLock в зависимости от того, вкл или выкл
+  function capsLock() {
+    if (caps === 'on') document.querySelector('.capsButton').classList.add('capsOn');
+    else document.querySelector('.capsButton').classList.remove('capsOn');
+  }
 };
 
 // console.log(`key`);
