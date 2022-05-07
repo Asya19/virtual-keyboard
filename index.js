9f2fecc493143198cf0aed43d41f66b284641d09
Asya19 <asya.kisko@yandex.ru>
2022-05-07 15:10:14 +0300

    feat: update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-window.onload = () => {
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable camelcase */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable guard-for-in */
+/* eslint-disable no-use-before-define */
+/* eslint-disable linebreak-style */
 
-  // Массив знаков на два языка
+window.onload = () => {
   const en = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
   const ru = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
-
-  // Массив знаков, когда зажат SHIFT
   const en_shift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
   const ru_shift = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '&uarr;', 'Shift', 'Ctrl', 'Win', 'Alt', '__________', 'Alt', '&larr;', '&darr;', '&rarr;', 'Ctrl'];
 
@@ -13,34 +17,38 @@ window.onload = () => {
 
   const BODY = document.querySelector('body');
 
-  let shiftState = false;
-  let capsState = false;
-  
+  const shiftState = false;
+  const capsState = false;
+
   let lang = 'en';
   let caps = 'off';
   let shift = false;
 
   CreateKeyboard();
-  let textArea = document.querySelector('textarea');
+  const textArea = document.querySelector('textarea');
 
-  // должно быть в классе
+  // eslint-disable-next-line no-multiple-empty-lines
+
   function CreateKeyboard() {
     languageCheck();
-    let content = `<textarea class="textArea"></textarea>
+    const CONTENT = `<textarea class="textArea"></textarea>
                   <div class="keyboard"></div>`;
-    BODY.insertAdjacentHTML('afterbegin', content);
+    BODY.insertAdjacentHTML('afterbegin', CONTENT);
 
-    let KEYBOARD = document.querySelector('.keyboard');
+    const KEYBOARD = document.querySelector('.keyboard');
 
-    for (let i in classForLengthBnt) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const i in classForLengthBnt) {
       const addClass = classForLengthBnt[i];
 
-      let content = `<div class="oneBtn key ${addClass}" data-code="${keyCodes[i]}" data-ru-shift="${ru_shift[i]}" data-en-shift="${en_shift[i]}" data-ru="${ru[i]}" data-en="${en[i]}">
+      // eslint-disable-next-line camelcase
+      // eslint-disable-next-line no-shadow
+      const CONTENT = `<div class="oneBtn key ${addClass}" data-code="${keyCodes[i]}" data-ru-shift="${ru_shift[i]}" data-en-shift="${en_shift[i]}" data-ru="${ru[i]}" data-en="${en[i]}">
       ${getLayOutKeyboard(i)}</div>`;
-      KEYBOARD.insertAdjacentHTML('beforeend', content);
+      KEYBOARD.insertAdjacentHTML('beforeend', CONTENT);
     }
 
-    let mes = `<p>Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe Shift + Alt<p>`;
+    const mes = '<p>Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe Shift + Alt<p>';
     BODY.insertAdjacentHTML('beforeend', mes);
   }
 
@@ -54,6 +62,7 @@ window.onload = () => {
 
   function getLayOutKeyboard(i) {
     if (shiftState === true || capsState === true) {
+      // eslint-disable-next-line camelcase
       if (lang === 'en') return en_shift[i];
       return ru_shift[i];
     }
@@ -85,14 +94,13 @@ window.onload = () => {
     if (dataCode === 'ShiftLeft' || dataCode === 'ShiftRight') updateKeyboard(event);
     if (dataCode === 'ArrowLeft' && cursor > 0) textArea.setSelectionRange(cursor - 1, cursor - 1);
 
-    if ((dataCode === 'AltLeft' && (event.shiftKey || shift)) || (dataCode === 'AltRight' && (event.shiftKey || shift)) || 
-        (dataCode === 'ShiftLeft' && event.altKey) || (dataCode === 'ShiftRight' && event.altKey)) {
+    if ((dataCode === 'AltLeft' && (event.shiftKey || shift)) || (dataCode === 'AltRight' && (event.shiftKey || shift))
+        || (dataCode === 'ShiftLeft' && event.altKey) || (dataCode === 'ShiftRight' && event.altKey)) {
       languageChange(event);
       removeShift(event);
     }
 
     if (dataCode === 'ArrowRight') {
-      console.log('ArrowRight');
       cursor = textArea.selectionEnd;
       textArea.setSelectionRange(cursor + 1, cursor + 1);
     }
@@ -100,17 +108,13 @@ window.onload = () => {
     if (dataCode === 'ArrowUp') {
       const textBeforeCursor = textArea.value.substring(0, cursor).split('\n');
 
-      // если нет вводов или строка длиннее 57 символов
-      if (textBeforeCursor.length === 1 || textBeforeCursor[textBeforeCursor.length - 1].length >= 57) 
-        cursor = -57;
+      // eslint-disable-next-line max-len
+      if (textBeforeCursor.length === 1 || textBeforeCursor[textBeforeCursor.length - 1].length >= 57) cursor = -57;
 
-      // если предыдущая строка длиннее позиции курсора в текущей строке
-      else if (textBeforeCursor[textBeforeCursor.length - 1].length <= textBeforeCursor[textBeforeCursor.length - 2].length % 57)
-        cursor -= (textBeforeCursor[textBeforeCursor.length - 2].length % 57) + 1;
-      
-      else
-        cursor -= textBeforeCursor[textBeforeCursor.length - 1].length + 1
-      
+      // eslint-disable-next-line max-len
+      else if (textBeforeCursor[textBeforeCursor.length - 1].length <= textBeforeCursor[textBeforeCursor.length - 2].length % 57) cursor -= (textBeforeCursor[textBeforeCursor.length - 2].length % 57) + 1;
+      else cursor -= textBeforeCursor[textBeforeCursor.length - 1].length + 1;
+
       if (cursor < 0) cursor = 0;
       textArea.setSelectionRange(cursor, cursor);
     }
@@ -121,27 +125,21 @@ window.onload = () => {
       const textBeforeCursor = textArea.value.substring(0, cursor).split('\n');
       const textAfterCursor = textArea.value.substring(textArea.selectionEnd).split('\n');
 
-      // если нет вводов или следующая строка длиннее 57 символов
       if (textAfterCursor.length === 1 || textAfterCursor[0].length >= 57) cursor += 57;
 
-      // если следующая строка короче позиции курсора
-      else if ((textBeforeCursor[textBeforeCursor.length - 1].length % 57) > textAfterCursor[1].length)
-        cursor += textAfterCursor[0].length + textAfterCursor[1].length + 1;
+      // eslint-disable-next-line max-len
+      else if ((textBeforeCursor[textBeforeCursor.length - 1].length % 57) > textAfterCursor[1].length) cursor += textAfterCursor[0].length + textAfterCursor[1].length + 1;
 
-      // если текущая строка очень длинная
-      else if ((((textBeforeCursor[textBeforeCursor.length - 1].length) + textAfterCursor[0].length) > 57))
-        cursor += textAfterCursor[0].length;
+      // eslint-disable-next-line brace-style
+      else if ((((textBeforeCursor[textBeforeCursor.length - 1].length) + textAfterCursor[0].length) > 57)) { cursor += textAfterCursor[0].length; }
 
-      // если следующая строка длиннее позиции курсора
-      else
-        cursor += (textBeforeCursor[textBeforeCursor.length - 1].length % 57) + textAfterCursor[0].length + 1;
+      else { cursor += (textBeforeCursor[textBeforeCursor.length - 1].length % 57) + textAfterCursor[0].length + 1; }
 
       textArea.setSelectionRange(cursor, cursor);
-
     }
 
     if ((!button.classList.contains('specialBtn')) && (!button.classList.contains('arrow'))) {
-      text = button.textContent.trim();
+      text = button.textCONTENT.trim();
       removeShift(event);
     }
 
@@ -164,14 +162,13 @@ window.onload = () => {
           cursor -= (cursor > 0) ? 1 : 1;
         } else cursor -= 1;
       }
-      
+
       textArea.value = textBeforeCursor + text + textAfterCursor;
       textArea.setSelectionRange(cursor + 1, cursor + 1);
       if (text === '    ') textArea.setSelectionRange(cursor + 4, cursor + 4);
     }
-    
   }
-  // должно быть в классе
+
   function changeCapsLock(event) {
     if (caps === 'on') caps = 'off';
     else caps = 'on';
@@ -183,20 +180,15 @@ window.onload = () => {
   function updateKeyboard(event) {
     if (event.shiftKey || shift) {
       document.querySelectorAll('.key').forEach((e) => {
-
         if (e.dataset[`${lang}Shift`] && !(e.classList.contains('specialBtn'))) {
           if (caps === 'on' && !(e.classList.contains('specialBtn'))) e.innerHTML = e.dataset[`${lang}Shift`].toLowerCase();
           else e.innerHTML = e.dataset[`${lang}Shift`];
-        }
-        else if (e.dataset[lang] && !(e.classList.contains('specialBtn'))) e.innerHTML = e.dataset[lang];
+        } else if (e.dataset[lang] && !(e.classList.contains('specialBtn'))) e.innerHTML = e.dataset[lang];
       });
-    }
-    else {
+    } else {
       document.querySelectorAll('.key').forEach((e) => {
         if (e.dataset[lang] && !(e.classList.contains('specialBtn'))) {
-          if (caps === 'on' && !(e.shiftKey || shift) && !(e.classList.contains('specialBtn'))) 
-            e.innerHTML = e.dataset[lang].toUpperCase();
-          else e.innerHTML = e.dataset[lang];
+          if (caps === 'on' && !(e.shiftKey || shift) && !(e.classList.contains('specialBtn'))) { e.innerHTML = e.dataset[lang].toUpperCase(); } else e.innerHTML = e.dataset[lang];
         }
       });
     }
@@ -211,13 +203,10 @@ window.onload = () => {
     }
   }
 
-  // меняет цвет текста capsLock в зависимости от того, вкл или выкл
   function capsLock() {
     if (caps === 'on') document.querySelector('.capsButton').classList.add('capsOn');
     else document.querySelector('.capsButton').classList.remove('capsOn');
   }
-
-
 
   document.addEventListener('keydown', (event) => {
     const currentButton = document.querySelector(`[data-code=${event.code}]`);
@@ -248,5 +237,4 @@ window.onload = () => {
       keyPress(event, currentButton, currentButton.dataset.code);
     }
   });
-
 };
